0cbb0e6606a85809147a3c9be9c8a36194a8ccf7
julienfa77 <faillaufaixjulien@gmail.com>
2019-04-02 13:43:55 +0200

    Preparation de l'administration

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class AdminController
+ * @IsGranted("ROULE_ADMIN")
+ */
 class AdminController extends AbstractController
 {
     /**
