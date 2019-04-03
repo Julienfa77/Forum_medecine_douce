09aa07801bcbabb6d105b191351f4666db976042
julienfa77 <faillaufaixjulien@gmail.com>
2019-04-03 10:10:17 +0200

    modif message 2

--- src/Form/MessageType.php
+++ src/Form/MessageType.php
@@ -3,7 +3,12 @@
 namespace App\Form;
 
 use App\Entity\Message;
+use App\Entity\Topic;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +17,16 @@ class MessageType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('titre')
-            ->add('contenu')
-            ->add('user')
-            ->add('topic')
+            ->add('titre',TextType::class)
+            ->add('contenu',TextareaType::class)
+            ->add('user',EntityType::class,[
+                'class'=>User::class,
+                'choice_label'=>'name'
+            ])
+            ->add('topic',EntityType::class,[
+                'class'=>Topic::class,
+                'choice_label'=>'titre'
+            ])
         ;
     }
 
@@ -23,6 +34,7 @@ class MessageType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Message::class,
+            'label_format'=>'message.%name%'
         ]);
     }
 }
