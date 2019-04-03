3b8b0b43807f7672f54e6006aa775499f5892cea
jura1990 <rajan.kumar@live.fr>
2019-04-03 11:58:56 +0200

    Rajout du DefaultController

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+use App\Entity\User;
+use Symfony\Component\Form\Extension\Core\Type\UserType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TexType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/home", name="home")
+     */
+    public function register(
+      Request $request,
+      EntityManagerInterface $manager,
+      UserPasswordEncoderInterface $encoder
+    ) {
+    $user = new User();
+    $form = $this->createFormBuilder($user)
+      ->add('email', EmailType::class)
+      ->add('password',PasswordType::class)
+      ->add('pseudo', TextType::class)
+      ->getForm();
+
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+      $user->setRoles(['ROLE_USER']);
+
+      $encoded = $encoder->encodePassword($user, $form['password']->getData());
+      $user->setPassword($encoded);
+
+      $manager->persist($user);
+      $manager->flush();
+      return $this->redirectToRoute('home');
+    }
+
+        return $this->render('home/register.html.twig'[
+          'register_form'=> $form->createView()
+        ]);
+    }
+}
