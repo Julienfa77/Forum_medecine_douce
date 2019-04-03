bad05a8a2b872e73f82eb46b395eccc080d91e17
julienfa77 <faillaufaixjulien@gmail.com>
2019-04-03 09:46:34 +0200

    Translation message

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +16,12 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-            ->add('name')
+            ->add('email', EmailType::class)
+            ->add('roles', CollectionType::class, [
+                'allow_add' => true
+            ])
+            ->add('password', PasswordType::class)
+            ->add('name',TextType::class)
         ;
     }
 
