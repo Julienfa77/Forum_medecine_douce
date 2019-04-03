016f01c7cf0cce3659e8e1e898a5a578cb84ae6e
jura1990 <rajan.kumar@live.fr>
2019-04-03 12:13:22 +0200

    Rajout register

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,13 +1,13 @@
 <?php
 
 namespace App\Controller;
+
 use App\Entity\User;
 use Symfony\Component\Form\Extension\Core\Type\UserType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\TexType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
@@ -19,33 +19,41 @@ class DefaultController extends AbstractController
     /**
      * @Route("/home", name="home")
      */
+    public function index()
+    {
+        return $this->render('default/index.html.twig');
+    }
+
+    /**
+     * @Route("/register", name="register")
+     */
     public function register(
       Request $request,
       EntityManagerInterface $manager,
       UserPasswordEncoderInterface $encoder
     ) {
-    $user = new User();
-    $form = $this->createFormBuilder($user)
-      ->add('email', EmailType::class)
-      ->add('password',PasswordType::class)
-      ->add('pseudo', TextType::class)
-      ->getForm();
+        $user = new User();
+        $form = $this->createFormBuilder($user)
+          ->add('email', EmailType::class)
+          ->add('password',PasswordType::class)
+          ->add('name', TextType::class)
+          ->getForm();
 
-    $form->handleRequest($request);
+        $form->handleRequest($request);
 
-    if ($form->isSubmitted() && $form->isValid()) {
-      $user->setRoles(['ROLE_USER']);
+        if ($form->isSubmitted() && $form->isValid()) {
+          $user->setRoles(['ROLE_USER']);
 
-      $encoded = $encoder->encodePassword($user, $form['password']->getData());
-      $user->setPassword($encoded);
+          $encoded = $encoder->encodePassword($user, $form['password']->getData());
+          $user->setPassword($encoded);
 
-      $manager->persist($user);
-      $manager->flush();
-      return $this->redirectToRoute('home');
-    }
+          $manager->persist($user);
+          $manager->flush();
+          return $this->redirectToRoute('home');
+        }
 
-        return $this->render('home/register.html.twig'[
-          'register_form'=> $form->createView()
+        return $this->render('default/register.html.twig', [
+          'register_form' => $form->createView()
         ]);
     }
 }
